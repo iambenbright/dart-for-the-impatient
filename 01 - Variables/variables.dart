4dfadbf88e7f4d30dc4c1e0d71f039df928c6548
iambenbright <yankeybenneth@gmail.com>
2020-07-18 00:53:51 +0000

    create and initialize variables

---
+++ 01 - Variables/variables.dart
@@ -0,0 +1,20 @@
+  // create and initialize variables
+
+main(List<String> args) {
+  // variables with inferred data type
+  var language = 'dart';  // `String`
+  dynamic score = 23;     // `int`
+  Object height = 11.5;   // `double`
+  const food = ['rice', 'milk', 'eggs'];    // `List<String>`
+  final isGreater = true;   // `bool`
+
+  // variables with explicit declared type
+  String name = 'Ben';
+  List<int> ages = [12, 9, 14];
+  int length = 67;
+  double width = 2.3;
+  Set<int> scores = {45, 67, 78};
+  Map<int, String> elements = {1: 'hydrogen', 2: 'Lithium', 17: 'chlorine'};
+  bool isOlder = false;
+  #name;  // creates Symbol type from name variable
+}
---
+++ 01 - Variables/variables.md
@@ -0,0 +1,17 @@
+# Variables
+
+- hold reference to data in memory
+- the type of data can be inferred or explicit declared
+
+## How to create and initialize variables
+
+To create a variable  `keyword variableName;`
+
+To create and initialize a variable `keyword variableName = value`;
+
+## Use cases of keywords
+
+| Use Case |  Keyword      |
+| :---     |  :---         |
+| data type is inferred    |  `var`, `dynamic`, `Object`, `const`, `final`  |
+| explicit declared type   | all built-in types  |
