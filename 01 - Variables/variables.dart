802e8b84df846aff7c12638844cdbe8591accb93
iambenbright <yankeybenneth@gmail.com>
2020-07-18 00:56:57 +0000

    how to create and initialize variables

--- 01 - Variables/variables.dart
+++ 01 - Variables/variables.dart
@@ -1,12 +1,12 @@
-  // create and initialize variables
+// create and initialize variables
 
 main(List<String> args) {
   // variables with inferred data type
-  var language = 'dart';  // `String`
-  dynamic score = 23;     // `int`
-  Object height = 11.5;   // `double`
-  const food = ['rice', 'milk', 'eggs'];    // `List<String>`
-  final isGreater = true;   // `bool`
+  var language = 'dart'; // `String`
+  dynamic score = 23; // `int`
+  Object height = 11.5; // `double`
+  const food = ['rice', 'milk', 'eggs']; // `List<String>`
+  final isGreater = true; // `bool`
 
   // variables with explicit declared type
   String name = 'Ben';
@@ -16,5 +16,5 @@ main(List<String> args) {
   Set<int> scores = {45, 67, 78};
   Map<int, String> elements = {1: 'hydrogen', 2: 'Lithium', 17: 'chlorine'};
   bool isOlder = false;
-  #name;  // creates Symbol type from name variable
+  #name; // creates Symbol type from name variable
 }
